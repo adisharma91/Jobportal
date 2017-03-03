808350845f81d14da8360ebf217aa99427da30b0
sharmarochan <sharmarochan@gmail.com>
2017-03-04 01:53:18 +0530

    ajax

--- static/jobsite/js/jobsite.js
+++ static/jobsite/js/jobsite.js
@@ -1,6 +1,55 @@
 //jobsite.js
 $(document).ready(function(){
+
 $('#myform').validationEngine({autoHidePrompt:true});
+
 $("#submitbtn").validationEngine('validate');
+
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+$( "#tags" ).autocomplete({
+  source: availableTags
+});
+
+$('.regis').keyup(function(){
+  var username = $("input[name='username']").val();
+ // alert(username);
+      $.ajax({
+            type:"POST",
+            url: "/check_user_exists_or_not/",
+            data: {
+                   'username': username,
+                  }
+
+            }).done(function(response){
+                  alert(response);
+                if (response == 'True') {
+                    alert(response);
+                }
+              });
+
+  });
 
 });
